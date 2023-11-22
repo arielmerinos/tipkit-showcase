14eb4f1a3672aae1152c6b9d0f612f7e5d818b42
arielmerinos <arielmerino@ciencias.unam.mx>
2023-11-21 22:33:28 -0600

    Initial Commit

---
+++ tipkit_demo/tipkit_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tipkit_demoApp.swift
+//  tipkit_demo
+//
+//  Created by Ariel Merino on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct tipkit_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
