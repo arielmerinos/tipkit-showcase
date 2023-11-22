b76baf5332d3fe37281914aa790731a9c567dedf
arielmerinos <arielmerino@ciencias.unam.mx>
2023-11-22 00:39:02 -0600

    innital commit

--- tipkit_demo/ContentView.swift
+++ tipkit_demo/ContentView.swift
@@ -8,15 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var colors = MockData.colors
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ScrollView{
+                ForEach(colors, id: \.self){
+                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                        .fill($0.gradient)
+                        .frame(height: 100)
+                        .contextMenu{
+                            Button("Favorite", systemImage: "star"){
+                                print("Favorite")
+                            }
+                            Button("Mark as done", systemImage: "checkmark"){
+                                print("Mark as done")
+                            }
+                        }
+                    
+                }
+            }
+            .padding()
+            .navigationTitle("Colors")
+            .toolbar{
+                Button{
+                    colors.insert(.random, at: 0)
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
         }
-        .padding()
     }
+}
+
+extension Color{
+    static var random: Color{
+        return Color(red:
+                        .random(in: 0...1),
+                     green:
+                        .random(in: 0...1),
+                     blue:
+                        .random(in: 0...1)
+        )
+    }
+}
+struct MockData{
+    static let colors = [Color.random,
+                         Color.random,
+                         Color.random,
+                         Color.random]
 }
 
 #Preview {
